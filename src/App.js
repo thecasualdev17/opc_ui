3c4a2c3e2d010e77aba7656ba88875c37f4f9fb7
Ronald Erosa <thecasualdev@gmail.com>
2021-06-12 02:06:21 +0800

    Added Axios and React Download Link
    Consumed OPC API using Axios
    Created Downloadable file using React Download Link

--- src/App.js
+++ src/App.js
@@ -1,29 +1,78 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
+import axios from "axios"
+import DownloadLink from "react-download-link";
 import './App.css';
 
 function App() {
   
-  const [link, setLink] = useState('some link asdasd');
-  const [printableRandomObjects, setprintableRandomObjects] = useState({});
+  const [link, setLink] = useState('');
+  const [alphabeticalStringsCount, setAlphabeticalStringsCount] = useState(0);
+  const [realNumbersCount, setRealNumbersCount] = useState(0);
+  const [integersCount, setIntegersCount] = useState(0);
+  const [alphanumericsCount, setAlphanumericsCount] = useState(0);
+  
+  const [printableRandomObjects, setprintableRandomObjects] = useState({
+    generatedString: "",
+    alphabeticalStringsCount: 0,
+    realNumbersCount: 0,
+    integersCount: 0,
+    alphanumericsCount: 0
+  });
 
   useEffect(() => {
     document.title = "Omnilytics Programming Challenge - UI"
   }, []);
 
+  const [isRequestPending, setIsRequestPending] = useState(false)
+  const sendRequest = useCallback(async () => {
+    setLink("")
+    if (isRequestPending) return
+    setIsRequestPending(true)
+    await axios(
+      'http://localhost:3000/generate',
+    ).then((res)=>{
+      setprintableRandomObjects(res.data);
+      makeFile(res.data.generatedString);
+    });
+    
+    setIsRequestPending(false)
+  }, [isRequestPending]);
+  
+  const createReport = function(){
+    setAlphabeticalStringsCount(printableRandomObjects.alphabeticalStringsCount);
+    setRealNumbersCount(printableRandomObjects.realNumbersCount);
+    setIntegersCount(printableRandomObjects.integersCount);
+    setAlphanumericsCount(printableRandomObjects.alphanumericsCount);
+  }
+
+  const makeFile = function(dataString){
+    const data = new Blob([dataString], { type: 'text/plain' })
+    if (link !== '') window.URL.revokeObjectURL(link)
+    setLink(window.URL.createObjectURL(data))
+  }
+
   return (
     <div className="app">
       <div className="contentWrapper">
-        <button onClick={() => alert('test')}>
+        <button className="btn" disabled={isRequestPending} onClick={sendRequest}>
             Generate
         </button>
-        <p>Link: <span className="appLink">{link}</span></p>
-        <button onClick={() => alert('test')}>
+        <p>Link:&ensp;
+        <DownloadLink
+            className="appLink"
+            disabled={isRequestPending}
+            label={link}
+            filename="Omnilytics Programming Challenge File"
+            exportFile={() => Promise.resolve(printableRandomObjects.generatedString)}
+        />
+        </p>
+        <button className="btn" onClick={createReport}>
           Report
         </button>
-        <p>Alphabetical String: {printableRandomObjects.AlphabeticalString}</p>
-        <p>Real Numbers: {printableRandomObjects.RealNumbers}</p>
-        <p>Integers: {printableRandomObjects.Integers}</p>
-        <p>Alphanumerics: {printableRandomObjects.Alphanumerics}</p>
+        <p>Alphabetical String: {alphabeticalStringsCount}</p>
+        <p>Real Numbers: {realNumbersCount}</p>
+        <p>Integers: {integersCount}</p>
+        <p>Alphanumerics: {alphanumericsCount}</p>
       </div>
     </div>
   );
